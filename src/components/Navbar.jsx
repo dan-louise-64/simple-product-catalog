198fd9be5288b809f435034050251ecbe8a27187
dan-louise-64 <danlou869@gmail.com>
2025-09-17 21:57:34 +0800

    Initialized Home and Sidebars

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+    <img src="" alt="" />
+    <ul>
+      <li>Home</li>
+      <li>Products</li>
+    </ul>
+    </div>
+
+  )
+}
+
+export default Navbar
